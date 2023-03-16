33060d19d12bfdc7a59f3ac430bf9273e231c65e
seif-khalled <seifkhalled684@gmail.com>
2023-03-16 01:21:21 -0700

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.>
+/**
+ * main - Entery point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzel");
+	return (0);
+}
