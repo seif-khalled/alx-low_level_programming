4d91f18af9957c7370cfe4deef8a3d34ff94fcac
seif-khalled <seifkhalled684@gmail.com>
2023-03-16 01:46:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.>
 /**
- * main - Entery point
+ * main - A program that prints a line with puts function
  *
- * Return: Always 0(Success)
+ * Return: 0(Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzel");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
